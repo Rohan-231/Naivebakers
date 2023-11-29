8b64b162944026f8bb3cefbd4ba96b44d4ccfd08
Devansh <97612790+hitdevansh@users.noreply.github.com>
2023-11-29 22:53:58 +0530

    Update settings.py

--- naivebaker/settings.py
+++ naivebaker/settings.py
@@ -132,10 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
